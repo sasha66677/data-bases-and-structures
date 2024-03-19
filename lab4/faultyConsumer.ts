7772b430a990814de0b94e6a2e408d502aaa3bbc
Alexander <123123salo21082018@gmail.com>
2024-03-20 02:16:29 +0300

    lab4: mongodb

---
+++ lab4/faultyConsumer.ts
@@ -0,0 +1,45 @@
+import { Connection, Message } from 'amqp-ts';
+
+async function delay(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+let shouldHandleNext = false;
+
+async function handleMessage(message: Message): Promise<void> {
+  if (shouldHandleNext) {
+    shouldHandleNext = false;
+    await delay(3000);
+  }
+
+  const content = message.getContent();
+  console.log('Получено сообщение', content);
+
+  if (content.number && content.number > 10) {
+    shouldHandleNext = true;
+    message.nack();
+    console.log('Сообщение не подтверждено');
+    return;
+  }
+  await delay(2000);
+  message.ack();
+  console.log('Сообщение подтверждено');
+}
+
+
+async function main(): Promise<void> {
+  try {
+    const connection = new Connection('amqp://localhost');
+    const queue = connection.declareQueue('queue', { durable: true });
+
+    queue.prefetch(1);
+
+    await connection.completeConfiguration();
+
+    await queue.activateConsumer(handleMessage);
+  } catch (error) {
+    console.error('Произошла ошибка при запуске программы:', error);
+  }
+}
+
+main();
